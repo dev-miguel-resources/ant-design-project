f751838d40cc65eb2c3f6386f27cdb6e2f5b0c9a
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-05-09 12:27:34 -0400

    migrate to 1.4.2 a 2.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ChartLine from './components/Line'
 function App() {
   return (
     <div className="App">
-      <h2>Ant Design Charts 1.4.2</h2>
+      <h2>Ant Design Charts 1.4.2 to 2.1.1</h2>
       <div>
         <ChartPie />;
         <ChartLine />;
